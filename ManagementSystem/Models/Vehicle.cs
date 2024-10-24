62a7b47aa2af45de37df4cf820d32752aaace31b
caionhocr <caioignacio086@gmail.com>
2024-10-24 16:07:04 -0300

    criação das models

---
+++ ManagementSystem/Models/Vehicle.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ManagementSystem.Models
+{
+    public class Vehicle
+    {
+        [Key]
+        public Guid VehicleId { get; set; }   
+
+        [Required]
+
+        public string VehicleModel { get; set; }
+        [Required]
+
+        public string VehicleLicensePlate { get; set; }
+        [Required]
+
+        public string VehicleColor { get; set;]
+            [Required]
+
+            public string VehicleBrand { get; set; }
+        [Required]
+
+        public Guid YardId { get; set; }
+        public Yard ? Yard {  get; set; }
+
+        [Required]
+        public string YardName { get; set;  
+    }
+}
